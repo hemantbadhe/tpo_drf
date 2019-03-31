d4756981950d17666124971edaf7f3ae0e4c1c1c
Hemant <hemantbbadhe@gmail.com>
2019-03-31 22:28:15 +0530

    build ready

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  var height= $( document ).height();
-  $(".login-image").css("height", height);
-  $(".pass-image").css("height", height);
-  $(".login-panel").css("height", height);
-  
-})(jQuery); // End of use strict
